40829f1c3762ddd5d7a7112b5a6c8655ae1245d2
TrevorW <TrevorW@VHI-3A-02>
2018-10-29 18:52:37 -0700

    awefewf

--- RiadosaOrg/Providers/Email.cs
+++ RiadosaOrg/Providers/Email.cs
@@ -18,24 +18,38 @@ namespace RiadosaOrg.Providers
                 string subject = request.Subject;
                 string body = request.Body;
 
-                var smtp = new SmtpClient
-                {
-                    Host = "smtp.gmail.com",
-                    Port = 587,
-                    EnableSsl = true,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    UseDefaultCredentials = false,
-                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-                };
-                using (var message = new MailMessage(fromAddress, toAddress)
-                {
-                    Subject = subject,
-                    Body = body
-                })
-                {
-                    smtp.Send(message);
-                }
+                //var smtp = new SmtpClient
+                //{
+                //    Host = "relay-hosting.secureserver.net",
+                //    Port = 25,
+                //    EnableSsl = true,
+                //    DeliveryMethod = SmtpDeliveryMethod.Network,
+                //    UseDefaultCredentials = true
+                //    //Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                //};
+                //using (var message = new MailMessage(fromAddress, toAddress)
+                //{
+                //    Subject = subject,
+                //    Body = body
+                //})
+                //{
+                //    smtp.Send(message);
+                //}
 
+
+
+
+
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("riadosaorg@gmail.com");
+
+                message.To.Add(new MailAddress("trevorcwilliams@gmail.com"));
+
+                message.Subject = request.Subject;
+                message.Body = "content of your email";
+
+                SmtpClient client = new SmtpClient();
+                client.Send(message);
                 return true;
             }
             catch(Exception ex)
