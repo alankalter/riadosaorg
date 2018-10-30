f13176ed9768396a0816360585baf3bbe171e533
TrevorW <TrevorW@VHI-3A-02>
2018-10-29 21:24:31 -0700

    asdfwef

--- RiadosaOrg/Providers/Email.cs
+++ RiadosaOrg/Providers/Email.cs
@@ -54,7 +54,7 @@ namespace RiadosaOrg.Providers
             }
             catch(Exception ex)
             {
-                throw ex;
+                //throw ex;
             }
         }
     }
